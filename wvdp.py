0097b66e7fce75bf70893d0daee2e9595ae9b64b
Pamela Wu <pam.wu@enigma.com>
2019-01-24 23:43:37 -0500

    refined callbacks

--- wvdp.py
+++ wvdp.py
@@ -142,13 +142,43 @@ def make_figure(nodes, edges, correlation_edges, pos):
 						}
 					}
 				}
+			} else {
+					for (var key in o) {
+					var vals = [];
+					for (var i = 0; i < o['start'].length; ++i) {
+						if ((Math.abs(o['r'][i]) > sv) && (cb.indexOf(o['type'][i]) > -1)) {
+							vals.push(o[key][i]);
+						}
+					}
+					e[key] = vals;
+				}
+				a['x_start'].length = 0;
+				a['y_start'].length = 0;
+				a['x_end'].length = 0;
+				a['y_end'].length = 0;
+				for (var i = 0; i < o['start'].length; ++i) {
+					if ((Math.abs(o['r'][i]) > sv) && (cb.indexOf(o['type'][i]) > -1)) {
+						if (o['e_arrow'][i] === 1) {
+							var l = o['xs'][i].length;
+							a['x_start'].push(o['xs'][i][l - 2]);
+							a['y_start'].push(o['ys'][i][l - 2]);
+							a['x_end'].push(o['xs'][i][l - 1]);
+							a['y_end'].push(o['ys'][i][l - 1]);
+						}
+						if (o['b_arrow'][i] === 1) {
+							a['x_start'].push(o['xs'][i][1]);
+							a['y_start'].push(o['ys'][i][1]);
+							a['x_end'].push(o['xs'][i][0]);
+							a['y_end'].push(o['ys'][i][0]);
+						}
+					}
+				}
 			}
       graph.edge_renderer.data_source.change.emit();
       arrow_source.change.emit();
   """)
   slider.js_on_change("value", callback)
   checkbox.js_on_change("active", callback)
-  
   graph.node_renderer.data_source.selected.js_on_change("indices", callback)
   
   hover = HoverTool(
